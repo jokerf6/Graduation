92341a5feb8651abd70edffeb8f7d6a8d9157212
jokerf6 <fhakem75@gmail.com>
2022-07-21 16:15:14 +0200

    test

--- util/connection.js
+++ util/connection.js
@@ -2,10 +2,10 @@ import { Sequelize } from 'sequelize';
 
 const connection = new Sequelize({
 	dialect: 'mysql',
-	host: 'localhost',
+	host: '196.205.94.85',
 	database: 'lms',
 	username: 'root',
-	password: 'Fahd11akem*',
+	password: '',
 	logging: false,
 });
 export { connection as default };
